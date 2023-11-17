8460bd1cd20ac7920429ff834abda716dfd4aece
Igor <igortendo@hotmail.com>
2023-11-17 13:35:35 -0300

    Refatorando o código para diminuir Ifs

--- Desafio2.js
+++ Desafio2.js
@@ -4,33 +4,25 @@ function contaDeVitorias(vitorias, derrotas){
     let saldoDeVitorias = vitorias - derrotas;
     let nivel = classificacao(saldoDeVitorias)
   
-    console.log(`O Herói tem um saldo de ${saldoDeVitorias} está no nivel ${nivel} `)
+    console.log(`O Herói tem um saldo de ${saldoDeVitorias} está no nível ${nivel} `)
   
   }
  
 // Divisão em 2 funções para boa prática onde a mesma classifica o nivel do herói  
 function classificacao(qtdVitorias){
-    let categoria;
+  //Arrays para a distribuição dos valores 
+  const limites = [10, 20, 50, 80, 90, 100];
+   const categorias = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"];
   
-    if(qtdVitorias < 10){
-      categoria = "Ferro"
-    }else if(qtdVitorias >= 11 && qtdVitorias <= 20){
-      categoria = "Bronze"
-    }else if(qtdVitorias >= 21 && qtdVitorias <= 50){
-      categoria = "Prata"
-    }else if(qtdVitorias >= 51 && qtdVitorias <= 80){
-      categoria = "Ouro"
-    }else if(qtdVitorias >= 81 && qtdVitorias <= 90){
-      categoria= "Diamante"
-    }else if(qtdVitorias >= 91 && qtdVitorias<= 100){
-      categoria= "Lendário"
-    }else{
-      categoria= "Imortal"
+   // Façando um for para contar os indices para encaixar na categoria desejada
+   for(let i = 0; i < limites.length; i++){
+    if(qtdVitorias <= limites[i]){
+      return categorias[i];
     }
-  
-    return categoria;
+   }
+   return "imortal";
   
   }
-  
-  
-    contaDeVitorias()
+
+  //Chamando a função  
+contaDeVitorias(50,6);
